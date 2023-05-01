19d8b71b75df4d6f0d97d5821d7acb2832ce83a6
Kegan Rodrigue <114108764+JackedProgrammer123@users.noreply.github.com>
2023-05-01 17:01:59 -0400

    new names

--- DotNetCoreSqlDb/Models/Todo.cs
+++ DotNetCoreSqlDb/Models/Todo.cs
@@ -3,12 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DotNetCoreSqlDb.Models
 {
-    public class Todo
+    public class Property
     {
         public int ID { get; set; }
-        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public string Cost { get; set; }
 
-        [Display(Name = "Created Date")]
+        [Display(Name = "Listed Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreatedDate { get; set; }
